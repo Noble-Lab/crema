a1a8310b4de89d8e1b9242347fd9e5a0593e3707
Donavan See <donavan@see-empire.com>
2020-11-29 13:16:25 -0800

    Update test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,13 +8,13 @@ import subprocess
 FILES = [
     os.path.join("data", f)
     for f in os.listdir("data")
-    if f.startswith("single")
+    if f.startswith("single_")
 ]
 
 
 def test_basic_cli(tmp_path):
     """Test that basic cli works."""
-    cmd = ["crema", FILES[1], "--output_dir", tmp_path]
+    cmd = ["crema", FILES[0], "--output_dir", tmp_path]
     subprocess.run(cmd, check=True)
     assert os.path.isfile(os.path.join(tmp_path, "crema.psm_results.txt"))
     assert os.path.isfile(os.path.join(tmp_path, "crema.logfile.log"))
