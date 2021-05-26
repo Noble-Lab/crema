041e60733804852f65a6d44f880f542e9815a2fb
Donavan See <donavan@see-empire.com>
2021-05-26 00:49:24 -0700

    Updated cli test
    Forgot to update these tests after changing log file output.

--- tests/system_tests/test_cli.py
+++ tests/system_tests/test_cli.py
@@ -8,7 +8,7 @@ def test_cli_basic(basic_crux_txt, tmp_path):
     cmd = ["crema", "--output_dir", tmp_path, "-e", "0.5", basic_crux_txt]
     subprocess.run(cmd, check=True)
     assert Path(tmp_path, "crema.psms.txt").exists()
-    assert Path(tmp_path, "crema.log").exists()
+    assert Path(tmp_path, "crema.log.txt").exists()
 
 
 def test_cli_custom_root(basic_crux_txt, tmp_path):
@@ -17,7 +17,7 @@ def test_cli_custom_root(basic_crux_txt, tmp_path):
     cmd.append(basic_crux_txt)
     subprocess.run(cmd, check=True)
     assert Path(tmp_path, "myFileRoot.crema.psms.txt").exists()
-    assert Path(tmp_path, "myFileRoot.crema.log").exists()
+    assert Path(tmp_path, "myFileRoot.crema.log.txt").exists()
 
 
 def test_real(real_crux_txt, tmp_path):
@@ -25,4 +25,4 @@ def test_real(real_crux_txt, tmp_path):
     cmd = ["crema", "--output_dir", tmp_path] + list(real_crux_txt)
     subprocess.run(cmd, check=True)
     assert Path(tmp_path, "crema.psms.txt").exists()
-    assert Path(tmp_path, "crema.log").exists()
+    assert Path(tmp_path, "crema.log.txt").exists()
